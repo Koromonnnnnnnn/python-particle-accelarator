ffa8ff2f83550c494d330b864915dbb1bae0bcb7
Koromonnnnnnnn <764363@dpsk12.net>
2024-01-31 09:26:06 -0700

    v1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 pygame.init()
 gamescreen = pygame.display.set_mode((900, 900))
 pygame.display.set_caption("Particle Accelerator")
@@ -13,14 +14,14 @@ xVel = []
 yVel = []  # velocity\
 
 # particles
-for i in range(10):
+for i in range(1000):
     xpos.append(450)
     ypos.append(450)
     xVel.append(random.randint(-2, 2))
     yVel.append(random.randint(-2, 3))
 
 while not gameOver:
-    clock.tick(60)
+    clock.tick(1000)
 
     # Event Handle
     for event in pygame.event.get():
@@ -28,20 +29,22 @@ while not gameOver:
             gameOver = True
 
     # physics
-    for i in range(10):
+    for i in range(1000):
         # update pos
         xpos[i] += xVel[i]
         ypos[i] += yVel[i]
 
       # boundary check
-        if xpos[i] < 0 or xpos[i] > 900:
-            xVel[i] *= -1
-        if ypos[i] < 0 or ypos[i] > 900:
-            yVel[i] *= -1
+        if xpos[i] < 0 or xpos[i] > 900 or ypos[i] < 0 or ypos[i] > 900:
+            xpos[i] = 450
+            ypos[i] = 450
+            xVel[i] = random.randint(-2, 2)
+            yVel[i] = random.randint(-2, 2)
 
     # render
     gamescreen.fill((0, 0, 0))
-    pygame.draw.circle(gamescreen, (255, 255, 255), (xpos[i], ypos[i]), 5)
+    for i in range(1000):
+        pygame.draw.circle(gamescreen, (255, 255, 255), (xpos[i], ypos[i]), 5)
     pygame.display.flip()
 
 # end of game
